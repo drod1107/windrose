0c223553942b2b2661e4f4aaa16d5e626a41eacb
David Rodriguez <davidrodrigueznolimit@gmail.com>
2024-07-21 11:37:08 -0500

    Revert "temp commit for troubleshooting schema error"
    This reverts commit 996e180bbf4b5b5f0256c3c468662ee7961dc44b.

--- app/[locale]/(routes)/components/ModuleMenu.tsx
+++ app/[locale]/(routes)/components/ModuleMenu.tsx
@@ -11,6 +11,7 @@ import ReportsModuleMenu from "./menu-items/Reports";
 import DocumentsModuleMenu from "./menu-items/Documents";
 import WindroseNavModuleMenu from "./menu-items/WindroseNav";
 import EmployeesModuleMenu from "./menu-items/Employees";
+import DataboxModuleMenu from "./menu-items/Databoxes";
 import CrmModuleMenu from "./menu-items/Crm";
 
 import AdministrationMenu from "./menu-items/Administration";
@@ -116,6 +117,11 @@ const ModuleMenu = ({ modules, dict }: Props) => {
               open={open}
               title={dict.ModuleMenu.documents}
             />
+          ) : null}
+          {modules.find(
+            (menuItem: any) => menuItem.name === "databox" && menuItem.enabled
+          ) ? (
+            <DataboxModuleMenu open={open} />
           ) : null}
           {modules.find(
             (menuItem: any) => menuItem.name === "openai" && menuItem.enabled
